bdc99d990e5533520d96dd7b321d0b4ffccc2d72
Joel Vardy <info@joelvardy.com>
2013-04-02 15:07:03 +0100

    Added templating library

--- application/routes/404.php
+++ application/routes/404.php
@@ -1,7 +1,13 @@
 <?php
 
+use Joelvardy\Template;
+
 $routes->notFound(function () {
 
-	require(VIEWS_PATH.'/404.php');
+	// Initialise classes
+	$template = new Template();
+
+	// Render the page
+	$template->render('404.php');
 
 });
