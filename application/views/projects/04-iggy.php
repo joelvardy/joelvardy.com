5f2a129ac339c7474f9b007d8d2265b566528b22
Joel Vardy <info@joelvardy.com>
2013-04-04 16:04:26 +0100

    Added IGGY project

---
+++ application/views/projects/04-iggy.php
@@ -0,0 +1,20 @@
+<div class="project">
+
+	<img class="header" src="/assets/img/projects/iggy-header.jpg" />
+
+	<div class="inner">
+
+		<h2>IGGY</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Contract Work</li>
+			<li><strong>Website:</strong> <a href="https://www.iggy.net/" title="TODO">iggy.net</a></li>
+			<li><strong>Date:</strong> April 2012 - October 2012</li>
+		</ul>
+
+		<p>While at Fish in a Bottle in Leamington Spa I worked on a project called IGGY, a website which is aimed at gifted 13 to 19 year olds, the website gives them a place to learn from academics and partake in challenges.</p>
+		<p>Myself and two other developers saw the project through from planning to conclusion.</p>
+
+	</div>
+
+</div>
