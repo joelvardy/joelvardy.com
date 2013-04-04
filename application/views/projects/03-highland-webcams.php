907f18850d75435d392b4445e81ccfbf991ccc6f
Joel Vardy <info@joelvardy.com>
2013-04-04 14:26:45 +0100

    Added Highland Webcams project

---
+++ application/views/projects/03-highland-webcams.php
@@ -0,0 +1,28 @@
+<div class="project">
+
+	<img class="header" src="/assets/img/projects/highland-webcams-header.jpg" />
+
+	<div class="inner">
+
+		<h2>Highland Webcams</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Side Project</li>
+			<li><strong>Website:</strong> <a href="http://highland-webcams.com" title="TODO">highland-webcams.com</a></li>
+			<li><strong>Date:</strong> August 2011 - September 2011 &#38; March 2013</li>
+		</ul>
+
+		<p>There are several live webcam feeds from which a still is taken and archived, this archive can then be accessed by users through the website.</p>
+		<p>Some of the challenges I faced with this project are:</p>
+		<ul>
+			<li>Archiving according to a unique schedule for each feed, some update every minute, 24 hours a day.</li>
+			<li>Handling slow downloads.</li>
+			<li>Unreliable webcam feeds, resulting in either images not being updated, or being corrupted.</li>
+			<li>Having to be as efficient as possible, and ensuring the archive is fast.</li>
+		</ul>
+		<p>The archive has more than <strong>1,700,000</strong> images archived, which is around 100GB stored on Amazon S3 (distributed through Cloud Front.)</p>
+		<p>This has been a side project I've been maintaining and updating for several years, over that time there have been <a href="http://highland-webcams.com/about" title="TODO">many iterations</a>.</p>
+
+	</div>
+
+</div>
