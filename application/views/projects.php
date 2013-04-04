75e702d6c6b6a8983f0315b04c3637c18124f161
Joel Vardy <info@joelvardy.com>
2013-04-04 10:37:15 +0100

    Styled projects page

--- application/views/projects.php
+++ application/views/projects.php
@@ -1,9 +1,38 @@
-<h1>Projects</h1>
+<!DOCTYPE html>
+<html lang="en-GB">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>TODO</title>
+		<meta name="description" content="TODO" />
+		<link rel="stylesheet" href="/assets/css/reset.css" />
+		<link rel="stylesheet" href="/assets/css/design.css" />
+	</head>
+	<body id="projects">
 
-<?php
+		<header>
+			<a href="/" title="TODO"><img src="/assets/img/joel-vardy-square.jpg" /></a>
+			<hgroup>
+				<h1><a href="/" title="TODO">Joel Vardy</a></h1>
+				<h4>Contract Web Developer</h4>
+			</hgroup>
+			<nav>
+				<a href="/" title="TODO">About Me</a>
+				<a class="active" href="/projects" title="TODO">My Projects</a>
+			</nav>
+		</header>
 
-	foreach ($projects as $project) {
-		echo $project;
-	}
+		<?php
 
-?>
+			foreach ($projects as $project) {
+				echo $project;
+			}
+
+		?>
+
+		<footer>
+			//footer//
+		</footer>
+
+	</body>
+</html>
--- application/views/projects/02-anything.php
+++ application/views/projects/02-anything.php
@@ -1,7 +1,11 @@
 <div class="project">
 
-	<h3>Project 2</h3>
+	<div class="inner">
 
-	<p>Duis pharetra, sem eu feugiat scelerisque, nulla ligula rutrum neque, eget blandit nisl turpis nec nulla. Maecenas pharetra, mi ac tincidunt aliquet, metus velit cursus nulla, vitae pretium eros diam ut magna. Vivamus sollicitudin euismod dictum. In lobortis tristique nibh, in placerat felis fermentum sit amet. Phasellus vehicula tristique turpis, eget laoreet leo auctor nec. Quisque vel leo in odio scelerisque sodales. Duis eu nulla lorem.</p>
+		<h2>Project 2</h2>
+
+		<p>Duis pharetra, sem eu feugiat scelerisque, nulla ligula rutrum neque, eget blandit nisl turpis nec nulla. Maecenas pharetra, mi ac tincidunt aliquet, metus velit cursus nulla, vitae pretium eros diam ut magna. Vivamus sollicitudin euismod dictum. In lobortis tristique nibh, in placerat felis fermentum sit amet. Phasellus vehicula tristique turpis, eget laoreet leo auctor nec. Quisque vel leo in odio scelerisque sodales. Duis eu nulla lorem.</p>
+
+	</div>
 
 </div>
