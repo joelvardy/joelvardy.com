7c7654462bf990c30544020cfbacc065a62e8b9b
Joel Vardy <info@joelvardy.com>
2013-04-04 16:48:06 +0100

    Added GIF It project

---
+++ application/views/projects/05-gifit.php
@@ -0,0 +1,18 @@
+<div class="project">
+
+	<div class="inner">
+
+		<h2>GIF It</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Side Project (hackathon)</li>
+			<li><strong>Date:</strong> February 2013</li>
+		</ul>
+
+		<p>I attended my second Summit Awesome hackathon, and developed a bot which would listen to mentions on Twitter, analyse the message and reply with an appropriate animated GIF.</p>
+		<p>I learnt how to work with the Twitter streaming API, and worked on ensuring the script (written in PHP) would continue to run uninterrupted.</p>
+		<p>I did not use a framework, the object oriented (PSR compliant) code is available on <a href="https://github.com/joelvardy/gif-bot" title="TODO">GitHub</a>.</p>
+
+	</div>
+
+</div>
