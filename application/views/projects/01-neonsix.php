40bd97ea48ff16c9092a384558211a14e11fe30d
Joel Vardy <info@joelvardy.com>
2013-04-04 11:43:56 +0100

    Added Neon Six project

---
+++ application/views/projects/01-neonsix.php
@@ -0,0 +1,20 @@
+<div class="project">
+
+	<img class="header" src="/assets/img/projects/neon-six-header.jpg" />
+
+	<div class="inner">
+
+		<h2>Neon Six</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Freelance</li>
+			<li><strong>Website:</strong> <a href="http://neonsix.com" title="TODO">neonsix.com</a></li>
+			<li><strong>Date:</strong> January 2012 - February 2012 &#38; August 2012</li>
+		</ul>
+
+		<p>Working with the in house designer I first created a static website, the design was quite simple although I had to use image replacement for some text, and I build a jQuery plugin to display a scrolling set of words at the top of the page.</p>
+		<p>Several months later I was asked to create a content management system for the website, I built this using CodeIgniter and the Ion Auth library for authentication, the CMS is quite basic but allows the company to manage news and work posts.</p>
+
+	</div>
+
+</div>
