7a31684dcb955be96d7c319668fcde3c43a58fde
Joel Vardy <info@joelvardy.com>
2013-04-04 13:29:00 +0100

    Added CI Auth Library project

---
+++ application/views/projects/02-codeigniter-authentication.php
@@ -0,0 +1,17 @@
+<div class="project">
+
+	<div class="inner">
+
+		<h2>CodeIgniter Authentication Library</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Side Project</li>
+			<li><strong>Date:</strong> September 2012</li>
+		</ul>
+
+		<p>I had built a few very simple projects using CodeIgniter when I decided to create my own basic authentication library, There are some projects where full blown access control lists aren't required, or where you want the authentication library to use an existing users table.</p>
+		<p>The library is quite simple, but well commented, and uses a very secure hashing algorithm. Take a look at <a href="https://github.com/joelvardy/Basic-CodeIgniter-Authentication" title="TODO">GitHub</a> for the source code.</p>
+
+	</div>
+
+</div>
