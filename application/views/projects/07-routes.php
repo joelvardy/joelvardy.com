89cbcc9e0b787ae6ddccb495ea61660a7592feb8
Joel Vardy <info@joelvardy.com>
2013-04-04 17:38:33 +0100

    Added routes project

---
+++ application/views/projects/07-routes.php
@@ -0,0 +1,17 @@
+<div class="project">
+
+	<div class="inner">
+
+		<h2>RESTful Routing</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Side Project</li>
+			<li><strong>Date:</strong> March 2013</li>
+		</ul>
+
+		<p>I developed a simple routing library which allows you write RESTful routes and map them to an anonymous function.</p>
+		<p>I have used this on several projects and decided to <a href="https://github.com/joelvardy/routes" title="TODO">open source</a> the code and add it to <a href="https://packagist.org/packages/joelvardy/routes" title="TODO">Packagist</a>.</p>
+
+	</div>
+
+</div>
