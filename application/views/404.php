9912b90361905a2f733c5dcdafa1539f062ad290
Joel Vardy <info@joelvardy.com>
2013-04-06 14:16:01 +0100

    Updated 404 page styling

--- application/views/404.php
+++ application/views/404.php
@@ -1,1 +1,41 @@
-Error 404 :(
+<!DOCTYPE html>
+<html lang="en-GB">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>TODO</title>
+		<meta name="description" content="TODO" />
+		<link rel="stylesheet" href="/assets/css/reset.css" />
+		<link rel="stylesheet" href="/assets/css/design.css" />
+		<link rel="stylesheet" href="/assets/css/error.css" />
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-27602018-2', 'joelvardy.com');
+			ga('send', 'pageview');
+		</script>
+	</head>
+	<body id="error-404">
+
+		<header>
+			<a href="/" title="About Joel Vardy"><img alt="My beautiful face" src="/assets/img/joel-vardy-square.jpg" /></a>
+			<nav>
+				<a href="/" title="About Joel Vardy">About Me</a>
+				<a href="/projects" title="Projects I've been involved in">My Projects</a>
+			</nav>
+		</header>
+
+		<div id="message">
+
+			<h2>HTTP/1.1 404 Not Found</h2>
+
+			<p>You know the drill, this page can't be found.. If you were looking for something really important contact me and I might be able to help :)</p>
+			<p>Regards,<br />
+			Joel</p>
+
+		</div>
+
+	</body>
+</html>
