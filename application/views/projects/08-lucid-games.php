75229cf08366a926a2c1fd93f51d01d1b4e7c21c
Joel Vardy <info@joelvardy.com>
2013-05-15 10:53:09 +0100

    Updated Lucid Games project

---
+++ application/views/projects/08-lucid-games.php
@@ -0,0 +1,20 @@
+<div class="project">
+
+	<img class="header" alt="iOS Game" src="/assets/img/projects/lucid-games-header.jpg" />
+
+	<div class="inner">
+
+		<h2>iOS Game</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Contract Work</li>
+			<li><strong>Date:</strong> October 2012 - Present</li>
+		</ul>
+
+		<p>My time at Lucid Games has been spent working on a currently unreleased iOS game.</p>
+		<p>Initially I was working on writing and maintaining object oriented javascript, I learnt a lot doing this, and it has lead on to me writing my own javascript applications and using node.js</p>
+		<p>As time went on I took up more of a consultant role, focused on the server side code, mainly relating to stability, and API features.</p>
+
+	</div>
+
+</div>
