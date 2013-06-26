f5527bf21c7e82a16ec1ef0fc3ec8b2cdeb5fafe
Joel Vardy <info@joelvardy.com>
2013-06-26 02:38:57 +0100

    Fixed markup typo

--- application/views/writing-list.php
+++ application/views/writing-list.php
@@ -35,7 +35,7 @@
 			<a class="post" href="/writing/<?php echo $post->slug; ?>" title="<?php echo $post->title; ?>">
 				<h2><?php echo $post->title; ?></h2>
 				<p><?php echo $post->intro; ?></p>
-				<p class="posted">Posted: <strong><?php echo date('jS F Y', $post->posted); ?></string></p>
+				<p class="posted">Posted: <strong><?php echo date('jS F Y', $post->posted); ?></strong></p>
 			</a>
 		<?php endforeach; ?>
 
