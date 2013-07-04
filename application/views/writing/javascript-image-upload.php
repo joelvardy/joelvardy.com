0e804b4e8602abb0844a876d9eccb8562d1af1dc
Joel Vardy <info@joelvardy.com>
2013-07-04 22:35:20 +0100

    Added size savings to JavaScript image upload post

--- application/views/writing/javascript-image-upload.php
+++ application/views/writing/javascript-image-upload.php
@@ -17,6 +17,36 @@
 	<h3>The Demo</h3>
 	<p>My demo uses vanilla JavaScript and will only work on modern browsers, it is available at <a href="http://demo.joelvardy.com/uploads/" title="JavaScript image upload demo" data-analytics="JavaScript image upload demo">demo.joelvardy.com/uploads</a> - upload some (large) images, and see what you think.</p>
 
+	<h3>The Benefits</h3>
+	<p>Because of the resizing there isn't a linear size saving, however the results of uploading several images are shown below:</p>
+	<table>
+		<thead>
+			<tr>
+				<td>Original</td>
+				<td>Uploaded</td>
+				<td>Saving</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>3.1MB</td>
+				<td>557KB</td>
+				<td>82%</td>
+			</tr>
+			<tr>
+				<td>5.6MB</td>
+				<td>318KB</td>
+				<td>94%</td>
+			</tr>
+			<tr>
+				<td>7.5MB</td>
+				<td>241KB</td>
+				<td>97%</td>
+			</tr>
+		</tbody>
+	</table>
+	<p>As you can see if someone uploads a 7.5MB image from their digital camera, there is a massive saving in upload time.</p>
+
 	<h3>The Source</h3>
 	<p>You can view / use the source code however you like, it is <a href="https://github.com/joelvardy/Javascript-image-upload" title="JavaScript image upload repository" data-analytics="JavaScript image upload repo">available on GitHub</a>.</p>
 
