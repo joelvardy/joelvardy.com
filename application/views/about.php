5b905f35c6340c199d66a4adda4265c6c4441a29
Joel Vardy <info@joelvardy.com>
2013-09-04 12:58:54 +0100

    Updated about me page

--- application/views/about.php
+++ application/views/about.php
@@ -33,11 +33,11 @@
 
 		<div id="biography">
 
-			<p>I'm a contract PHP developer working in the UK (although I'm interested in working abroad.) You can see my skills at the bottom of this page, or view <a href="/projects" title="Projects I've been involved in">my projects and work</a>.</p>
+			<p>I'm a contract web developer (both front and back end code) working in the UK, my skills are listed at the bottom of this page. If you would like you can view <a href="/projects" title="Projects I've been involved in">my projects and work</a>.</p>
 			<p>When I'm not sat in front of Sublime Text:</p>
 			<ul>
 				<li>I might be <strong>skiing</strong> or <strong>snowboarding</strong>, both I've been doing for years.</li>
-				<li>I could be in the countryside <strong>mountain biking</strong>, I've attended the annual Fort William World Cup since 2010.</li>
+				<li>Getting muddy <strong>mountain biking</strong>, local rides in the Peak District or riding further afield.</li>
 				<li>While doing these things it's likely I'll be also indulging in my love for  <strong>photography</strong>.</li>
 			</ul>
 
@@ -53,8 +53,8 @@
 
 			<h4>Server Administration</h4>
 			<ul>
-				<li>Linux server setup, specifically <strong>Ubuntu</strong>, also usage of <strong>CentOS 5</strong>.</li>
-				<li>Knowledge of installation, configuration and usage of: <strong>Apache 2</strong>, <strong>Nginx</strong>, <strong>MySQL</strong>, <strong>APC</strong>, and <strong>Memcached</strong>.</li>
+				<li>Linux server setup, specifically <strong>Debian / Ubuntu</strong>, also usage of <strong>CentOS 5</strong>.</li>
+				<li>Knowledge of installation and configuration of: <strong>PHP</strong>, <strong>Apache</strong>, <strong>Nginx</strong>, <strong>MySQL</strong>, and <strong>Memcached</strong>.</li>
 			</ul>
 
 			<h4>Version Control</h4>
@@ -83,7 +83,7 @@
 				<li>I write semantic markup and have a good understanding of <strong>HTML5</strong> elements.</li>
 				<li>I believe in progressive enhancement, using <strong>CSS3</strong> where possible.</li>
 				<li>Experience of developing <strong>responsive designs</strong>.</li>
-				<li>A good knowledge of <strong>jQuery</strong>.</li>
+				<li>A good knowledge of <strong>jQuery</strong> - although I know when and how to use vanilla JavaScript.</li>
 			</ul>
 
 		</div>
@@ -95,7 +95,7 @@
 			<p>The best ways to get in touch are:</p>
 			<ul>
 				<li><a href="https://twitter.com/joelvardy" title="My Twitter profile" data-analytics="Twitter profile">@joelvardy</a> - Twitter is usually the quickest method.</li>
-				<li><a href="mailto:info@joelvardy.com" title="Send me an email">info@joelvardy.com</a> - sometimes email is just easier.</li>
+				<li><a href="mailto:info@joelvardy.com" title="Send me an email">info@joelvardy.com</a> - for when you can't fit it into 140 characters.</li>
 			</ul>
 
 		</div>
