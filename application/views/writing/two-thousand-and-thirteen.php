6809bf830efbeba3819fab87257ec35bb2bd88b8
Joel Vardy <info@joelvardy.com>
2014-01-02 13:49:19 +0000

    Started working on 2013 post

---
+++ application/views/writing/two-thousand-and-thirteen.php
@@ -0,0 +1,19 @@
+<div class="post">
+
+	<h2>Two Thousand and Thirteen</h2>
+	<h4>Posted: <date>1st January 2014</date></h4>
+
+	<p>I've done so much in two thousand and thirteen; <strong>good</strong>, <strong>bad</strong>, <strong>sad</strong>, <strong>happy</strong>, <strong>important</strong>, and <strong>trivial</strong>. I'm going to keep a record of these things here, hopefully I'll look back at the list when I'm a little more decrepit.</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/joel-vardy-full-width.jpg" />
+		<div class="inner">
+			<time datetime="2013-01-12">12<sup>th</sup> Jan</time>
+			<div class="details">
+				<h5>Bought a MacBook Pro</h5>
+				<p>I was working at <a href="/projects#2k-drive" title="2K Drive">Lucid Games</a> where I was provided a Mac Mini and an iPad, I enjoyed using the Mac so much I decided to join the darkside.</p>
+			</div>
+		</div>
+	</div>
+
+</div>
