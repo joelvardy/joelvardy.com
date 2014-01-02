fd672cb2afc0f79290d41882047a237ed756d0bd
Joel Vardy <info@joelvardy.com>
2014-01-02 11:59:45 +0000

    Added SMRS to projects

---
+++ application/views/projects/09-smrs.php
@@ -0,0 +1,19 @@
+<div id="smrs" class="project">
+
+	<img class="header" alt="SMRS" src="/assets/img/projects/smrs-header.jpg" />
+
+	<div class="inner">
+
+		<h2>SMRS</h2>
+
+		<ul class="details">
+			<li><strong>Project Type:</strong> Contract Work</li>
+			<li><strong>Website:</strong> <a href="http://smrs.co.uk/" title="SMRS" data-analytics="SMRS">smrs.co.uk</a></li>
+			<li><strong>Date:</strong> December 2013 - Present</li>
+		</ul>
+
+		<p>While at SMRS I was involved in a large number of web projects, these ranged from simple websites, to content managed solutions, and custom integrations.</p>
+
+	</div>
+
+</div>
