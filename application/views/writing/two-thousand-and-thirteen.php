6b58713fd2a423bf080586cc0d601108577956b7
Joel Vardy <info@joelvardy.com>
2014-01-02 22:30:09 +0000

    Added some more events to 2013 post

--- application/views/writing/two-thousand-and-thirteen.php
+++ application/views/writing/two-thousand-and-thirteen.php
@@ -18,7 +18,9 @@
 		</div>
 	</div>
 
-	<p>TODO</p>
+	<p>I attended the second Summit Awesome hackathon in Mnachester, and while still high on energy drink put together <a href="https://vimeo.com/joelvardy/sa-hack-man-2013" title="Manchester Hackathon Timelapse">this timelapse</a>.</p>
+
+	<p>Straight after the 24 hour hackathon in Manchester I traveled directly to Aviemore in the Scottish Highlands, I was exhaused when I arrived, but the beautiful weather made up for it. <strong>I even got out on my snowboard!</strong></p>
 
 	<div class="photo full-width event">
 		<img src="/assets/img/2013/skiing.jpg" />
@@ -26,6 +28,84 @@
 			<time datetime="2013-02-18"><span class="top">18<sup>th</sup></span><span class="bottom">Feb</span></time>
 			<div class="details">
 				<h5>Enjoyed the Awesome Scottish Snow</h5>
+				<p>TODO</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/industry-conf.jpg" />
+		<div class="inner">
+			<time datetime="2013-04-24"><span class="top">24<sup>th</sup></span><span class="bottom">Apr</span></time>
+			<div class="details">
+				<h5>Attended First Conference</h5>
+				<p>I was very happy to attend Industry Conf in Newcastle.</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/berlin.jpg" />
+		<div class="inner">
+			<time datetime="2013-05-10"><span class="top">10<sup>th</sup></span><span class="bottom">May</span></time>
+			<div class="details">
+				<h5>Berlin</h5>
+				<p>I visited Berlin twice in 2013, the first time was in May, the second, in August</p>
+			</div>
+		</div>
+	</div>
+
+	<p>I took time out from work, to cycle over to Derwent reservoir and whitness a Lancaster bomber perform a flypast.</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/scottish-roadtrip.jpg" />
+		<div class="inner">
+			<time datetime="2013-06-09"><span class="top">9<sup>th</sup></span><span class="bottom">Jun</span></time>
+			<div class="details">
+				<h5>Scottish Roadtrip</h5>
+				<p>TODO</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/kinky-kaylee.jpg" />
+		<div class="inner">
+			<time datetime="2013-10-23"><span class="top">24<sup>th</sup></span><span class="bottom">Jun</span></time>
+			<div class="details">
+				<h5>New Bike</h5>
+				<p>While at the Fort William Mountain Bike World Cup, I purchaced a new bike, she is called Kinky Kaylee :)</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/full-face.jpg" />
+		<div class="inner">
+			<time datetime="2013-10-23"><span class="top">23<sup>rd</sup></span><span class="bottom">Oct</span></time>
+			<div class="details">
+				<h5>Back on my Bike</h5>
+				<p>TODO</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/hovercrafting.jpg" />
+		<div class="inner">
+			<time datetime="TODO"><span class="top">00<sup>rd</sup></span><span class="bottom">ADD</span></time>
+			<div class="details">
+				<h5>Piloting a Hovercraft</h5>
 				<p>TODO</p>
 			</div>
 		</div>
