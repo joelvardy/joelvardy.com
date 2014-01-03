ab29fa1c726610ed93e519cb1b49db128de13dac
Joel Vardy <info@joelvardy.com>
2014-01-03 08:15:51 +0000

    Added more events to 2013 post

--- application/views/writing/two-thousand-and-thirteen.php
+++ application/views/writing/two-thousand-and-thirteen.php
@@ -75,12 +75,66 @@
 	<p>TODO</p>
 
 	<div class="photo full-width event">
+		<img src="/assets/img/2013/of-tales-untold.jpg" />
+		<div class="inner">
+			<time datetime="2013-07-13"><span class="top">13<sup>th</sup></span><span class="bottom">Jul</span></time>
+			<div class="details">
+				<h5>Of Tales Untold</h5>
+				<p>I went to a mini festival called <strong>Of Tales Untold</strong> in TODO.</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
 		<img src="/assets/img/2013/kinky-kaylee.jpg" />
 		<div class="inner">
 			<time datetime="2013-10-23"><span class="top">24<sup>th</sup></span><span class="bottom">Jun</span></time>
 			<div class="details">
 				<h5>New Bike</h5>
 				<p>While at the Fort William Mountain Bike World Cup, I purchaced a new bike, she is called Kinky Kaylee :)</p>
+			</div>
+		</div>
+	</div>
+
+	<p>I saw Fall Out Boy in Hamburg, I have been a fan for years, and was so happy when I heard they they were ending their hiatus late 2012, it was awesome to finally see them live!</p>
+
+	<p>While in Germany, I planned to go to Leeds Fest, the first <strong>real</strong> festival I've been to. The mud was horrific, the bands were great, lugging our stuff back across the ruined fields back to the bus was a struggle. But my highlight by far (as all of my friends know) was seeing Eminem perform live!</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/orkney.jpg" />
+		<div class="inner">
+			<time datetime="2013-08-15"><span class="top">15<sup>th</sup></span><span class="bottom">Aug</span></time>
+			<div class="details">
+				<h5>Orkney</h5>
+				<p>TODO</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/prague.jpg" />
+		<div class="inner">
+			<time datetime="2013-08-31"><span class="top">31<sup>st</sup></span><span class="bottom">Aug</span></time>
+			<div class="details">
+				<h5>Czech Republic</h5>
+				<p>TODO</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
+		<img src="/assets/img/2013/mtb-crash.jpg" />
+		<div class="inner">
+			<time datetime="2013-09-29"><span class="top">29<sup>rd</sup></span><span class="bottom">Sep</span></time>
+			<div class="details">
+				<h5>MTB Crash</h5>
+				<p>TODO</p>
 			</div>
 		</div>
 	</div>
@@ -101,14 +155,29 @@
 	<p>TODO</p>
 
 	<div class="photo full-width event">
+		<img src="/assets/img/2013/bonfire.jpg" />
+		<div class="inner">
+			<time datetime="2013-11-02"><span class="top">2<sup>nd</sup></span><span class="bottom">Nov</span></time>
+			<div class="details">
+				<h5>Bonfire</h5>
+				<p>TODO</p>
+			</div>
+		</div>
+	</div>
+
+	<p>TODO</p>
+
+	<div class="photo full-width event">
 		<img src="/assets/img/2013/hovercrafting.jpg" />
 		<div class="inner">
-			<time datetime="TODO"><span class="top">00<sup>rd</sup></span><span class="bottom">ADD</span></time>
+			<time datetime="2013-12-14"><span class="top">14<sup>th</sup></span><span class="bottom">Dec</span></time>
 			<div class="details">
 				<h5>Piloting a Hovercraft</h5>
 				<p>TODO</p>
 			</div>
 		</div>
 	</div>
+
+	<p>I love getting something to build on Christmas day, this year I recieved a Lego Technic service truck, I created a timelapse of the <a href="TODO" title="TODO">build process</a>.</p>
 
 </div>
