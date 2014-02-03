7a74ad79ebc87bc8873e5bf58ea7d6be5c7bef73
Joel Vardy <info@joelvardy.com>
2014-02-03 03:21:05 +0000

    Added Summit Awesome hackathon post

---
+++ application/views/writing/summit-awesome-hackathon-february-2014.php
@@ -0,0 +1,38 @@
+<div class="post">
+
+	<h2>Summit Awesome Hackathon February 2014</h2>
+	<h4>Posted: <date>3rd February 2014</date></h4>
+
+	<p>Almost a year on from the last Summit Awesome hackathon, we are back in Manchester, at the TechHub, for another two day hackathon with some great, like-minded people.</p>
+	<p>There have been two Summit Awesome hackathons previous to this one, after both I think everyone has been in agreement that doing an all nighter is a pretty stupid idea. But did I book into a local hotel room, of course not. Did I bring my amazing <a href="https://twitter.com/joelvardy/status/381031947029340160">MusucBag</a>, no I didn't.</p>
+
+	<div class="photo full-width">
+		<img src="/assets/img/writing/summit-awesome-hackathon-february-2014.jpg" />
+	</div>
+
+	<h3>The Projects</h3>
+	<p>There were several projects being worked on between the 25 people, the ones I remember:</p>
+	<ul>
+		<li><strong>CMYK</strong>: Real-time design feedback, <a href="http://cmyk.im">cmyk.im</a>.</li>
+		<li><strong>XinPet</strong>: Managing pets microchips.</li>
+		<li><strong>Chekk.it</strong>: Find the status of Minecraft plugins.</li>
+	</ul>
+	<p>No wanting to commit time to a new projects, I was working on finishing one of my own existing personal projects.</p>
+
+	<h3>Timelapse</h3>
+	<p>I really love timelapses, I set one up last year, and it was really well accepted, so I did one again this year.</p>
+	<p>When I was trying to make the edit more interesting, I realised I should have taken some clips of people typing away ect.. But hey, I'm still learning and trying new things.</p>
+
+	<div class="video aspect-10-9">
+		<iframe src="//player.vimeo.com/video/85689899?title=0&amp;byline=0&amp;portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	</div>
+
+	<h3>The Burnout</h3>
+	<p>I can't actually remember the last time I did an all nighter, so by 4am I was really feeling the lack of sleep. With almost everyone else asleep, I looked at what time the next train to Sheffield was, this set the seed of an idea, and I did decide to get the first train back home in the morning.</p>
+	<p>This meant I unfortunately missed the presentations on Sunday afternoon, but I have to say my bed has never been so comfy!</p>
+
+	<h3>Next Time</h3>
+	<p>I really hope next time the days are forcibly spilt up, it would be additional cost for people who don't want to pay for somewhere to stay, but I think everyone would be more awake (and actually in attendance) on the Sunday.</p>
+	<p>I really recommend attending a Summit Awesome meet up, either for food, drinks, or a hackathon. Find out more at <a href="http://summitawesome.co.uk/" title="Summit Awesome" data-analytics="Summit Awesome">summitawesome.co.uk</a>.</p>
+
+</div>
