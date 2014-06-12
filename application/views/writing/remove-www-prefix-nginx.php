7b63fe4bf6261a81e0233c68d48df00c1e0d19fe
Joel Vardy <info@joelvardy.com>
2014-06-12 10:06:46 +0100

    Changed remove www post title

--- application/views/writing/remove-www-prefix-nginx.php
+++ application/views/writing/remove-www-prefix-nginx.php
@@ -1,13 +1,13 @@
 <div class="post">
 
-	<h2>Remove WWW (nginx)</h2>
+	<h2>Remove WWW Prefix (nginx)</h2>
 	<h4>Posted: <date>12th June 2014</date></h4>
 
 	<p>Some people like domains to have a www. prefix, I don't I find it an unnecessary relic of the <em>old web</em>. It's also bad practice to be serving the same content over both www.domain.tld and domain.tld</p>
 	<p>I'm going to show you how to write one rule which will redirect all www. prefixed domains to the plain domain. I am using nginx as my web server, I don't think this is as easy with Apache (however this is probably possible with a default vhost and some rewrites.)</p>
 
 	<h3>New Virtual Host</h3>
-	<p>The following will listen for all requests via HTTP and HTTPS with start with www. and are not matched by other virtual hosts. It will then redirect them to the HTTP or HTTP domain (including the URI) without the www. prefix.</p>
+	<p>The following will listen for all requests which start with www. and are not matched by other virtual hosts. It will then redirect them to the domain (including the URI) without the www. prefix.</p>
 <pre><code class="language-markup">server {
 	listen 80;
 	listen 443 ssl;
