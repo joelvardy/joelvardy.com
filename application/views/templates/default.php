dedf67801977abd7f7f2fd9a6d9c498d05e6ba94
Joel Vardy <info@joelvardy.com>
2014-09-25 15:59:59 +0100

    Removed Google authorship, and updated GA tracking code

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -13,12 +13,8 @@
 		<title><?php echo $template->title; ?></title>
 		<meta name="description" content="<?php echo $template->description; ?>" />
 		<link rel="stylesheet" href="/assets/minified/design.css" />
-		<link rel="author" href="https://plus.google.com/102110732747129499789" />
 		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 			ga('create', 'UA-27602018-2', 'joelvardy.com');
 			ga('send', 'pageview');
 		</script>
