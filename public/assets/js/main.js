988fb10c1bba0ed537f79d1f00aaab5c73f57814
Joel Vardy <info@joelvardy.com>
2014-10-24 15:00:06 +0100

    Fixed full width image in Firefox

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -99,7 +99,7 @@ window.addEventListener('load', function(event) {
 
 			// Ensure photos are full width
 			var body = document.querySelector('body'),
-				bodyMargin = Math.ceil(parseFloat(window.getComputedStyle(body).marginLeft));
+				bodyMargin = Math.ceil((document.documentElement.clientWidth - parseFloat(window.getComputedStyle(document.body).width)) / 2);
 			imageContainer.style.marginLeft = -bodyMargin+'px';
 			imageContainer.style.marginRight = -bodyMargin+'px';
 
