fef3f2152fe779fc73e4bba590e78614d3614dae
Joel Vardy <info@joelvardy.com>
2015-02-11 15:32:59 +0000

    Updated Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,52 @@
 // Load plugins
-var gulp = require('gulp'),
-	del = require('del'),
-	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
+var del = require('del'),
+	gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
-	uglify = require('gulp-uglifyjs');
+	minifyCSS = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
 
 
-// SASS
+// Styles
 gulp.task('styles', function () {
-	return gulp.src('public/assets/sass/design.scss')
-	.pipe(sass({
-		style: 'compressed'
-	}))
-	.on('error', notify.onError(function (error) {
-		return error.message;
-	}))
-	.pipe(gulp.dest('public/assets/minified'))
-	.pipe(notify({
-		title: 'Styles',
-		message: 'Task complete'
-	}));
+
+	return sass('./public/assets/sass/design.scss')
+	.pipe(minifyCSS())
+	.on('error', function (error) {
+		console.error('Error!', error);
+	})
+	.pipe(gulp.dest('./public/assets/minified'));
+
 });
 
 
 // JavaScript
 gulp.task('scripts', function () {
+
 	return gulp.src('public/assets/js/**/*.js')
-	.pipe(concat('main.js'))
-	.pipe(gulp.dest('public/assets/minified'))
-	.pipe(uglify({
-		outSourceMap: true
-	}))
-	.pipe(gulp.dest('public/assets/minified'))
-	.pipe(notify({
-		title: 'Scripts',
-		message: 'Task complete'
-	}));
+	.pipe(concat('app.js'))
+	.pipe(uglify())
+	.on('error', function (error) {
+		console.log(error);
+	})
+	.pipe(gulp.dest('./public/assets/minified'));
+
 });
 
 
 // Clean
-gulp.task('clean', function() {
-	del(['public/assets/minified']);
+gulp.task('clean', function () {
+	del(['./public/assets/minified/*']);
 });
 
 
 // Default task
-gulp.task('default', ['clean'], function() {
+gulp.task('default', ['clean'], function () {
 	gulp.start('styles', 'scripts');
 });
 
 // Watch
-gulp.task('watch', function() {
-	gulp.watch('public/assets/sass/**/*.scss', ['styles']);
-	gulp.watch('public/assets/js/**/*.js', ['scripts']);
+gulp.task('watch', function () {
+	gulp.watch('./public/assets/sass/**/*.scss', ['styles']);
+	gulp.watch('./public/assets/js/**/*.js', ['scripts']);
 });
