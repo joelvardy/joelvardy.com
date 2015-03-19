204b0fd8ff3aa89005749d5c2a89cba397b5e27c
Joel Vardy <info@joelvardy.com>
2015-03-19 01:51:15 +0000

    Try using asynchronous CSS and JavaScript

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -12,8 +12,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<title><?php echo $title; ?></title>
 		<meta name="description" content="<?php echo $description; ?>" />
-		<link rel="stylesheet" href="/assets/minified/design.css" />
+		<link async rel="stylesheet" href="/assets/minified/design.css" />
 		<link rel="publisher" href="https://plus.google.com/+JoelVardyDeveloper" />
+		<script async src="/assets/minified/app.js"></script>
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 			ga('create', 'UA-27602018-2', 'joelvardy.com');
@@ -24,6 +25,5 @@
 
 		<?php echo $page_view; ?>
 
-		<script src="/assets/minified/app.js"></script>
 	</body>
 </html>
