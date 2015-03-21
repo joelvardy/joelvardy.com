6fd775a3253294cd9837c0b9792e9a72f8e808da
Joel Vardy <info@joelvardy.com>
2015-03-21 13:44:21 +0000

    Reverted async script attribute

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -14,7 +14,6 @@
 		<meta name="description" content="<?php echo $description; ?>" />
 		<link async rel="stylesheet" href="/assets/minified/design.css" />
 		<link rel="publisher" href="https://plus.google.com/+JoelVardyDeveloper" />
-		<script async src="/assets/minified/app.js"></script>
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 			ga('create', 'UA-27602018-2', 'joelvardy.com');
@@ -25,5 +24,6 @@
 
 		<?php echo $page_view; ?>
 
+		<script src="/assets/minified/app.js"></script>
 	</body>
 </html>
