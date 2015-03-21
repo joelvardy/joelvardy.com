a6e56eb592dc9a066e953554725d4652269787e0
Joel Vardy <info@joelvardy.com>
2015-03-21 14:56:03 +0000

    Added social profiles to structured data

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -13,7 +13,6 @@
 		<title><?php echo $title; ?></title>
 		<meta name="description" content="<?php echo $description; ?>" />
 		<link async rel="stylesheet" href="/assets/minified/design.css" />
-		<link rel="publisher" href="https://plus.google.com/+JoelVardyDeveloper" />
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 			ga('create', 'UA-27602018-2', 'joelvardy.com');
@@ -33,7 +32,12 @@
 				"address": {
 					"@type": "PostalAddress",
 					"addressCountry": "GB"
-				}
+				},
+				"sameAs" : [
+					"https://www.linkedin.com/in/joelvardy",
+					"https://twitter.com/joelvardy",
+					"https://plus.google.com/+JoelVardyDeveloper"
+				]
 			}
 		</script>
 	</head>
