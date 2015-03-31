012d2d014fb7086992debf29d5d8108d2afab1b2
Joel Vardy <info@joelvardy.com>
2015-03-31 12:35:21 +0100

    Updated chatting.im project

--- application/views/projects/07-chatting.php
+++ application/views/projects/07-chatting.php
@@ -9,11 +9,11 @@
 		<ul class="details">
 			<li><strong>Project Type:</strong> Side Project</li>
 			<li><strong>Website:</strong> <a href="https://chatting.im/" title="Chatting" data-analytics="Chatting">chatting.im</a></li>
-			<li><strong>Date:</strong> April 2013</li>
+			<li><strong>Date:</strong> April 2013 (major refactor in March 2015)</li>
 		</ul>
 
-		<p>One of the first things I built with node.js this has been slightly updated recently, although it still retains the original concept.</p>
-		<p>You create a <em>room</em> with an encryption passphrase, your friends sign into this <em>room</em> and your messages are encrypted and decrypted client side, preventing anyone reading the messages while in transit, or on the server.</p>
+		<p>As one of the first things I built with node.js I kept it simple, but added a twist to the standard "messaging system" by encrypting all messages locally in the users browser. The encryption key is stored in the URL so by sharing the URL with friends you can securely send messages.</p>
+		<p>If you would like to see how I have built the website, please review the <a href="https://github.com/joelvardy/chatting.im" title="Open source repository" data-analytics="chatting.im repo">the source code</a> on GitHub.</p>
 
 	</div>
 
