3a98446da6c05e1c01c31629b4aeaa58b695b5ca
Joel Vardy <info@joelvardy.com>
2015-08-25 12:02:19 +0100

    Updated chatting.im project

--- application/views/projects/05-chatting.php
+++ application/views/projects/05-chatting.php
@@ -12,8 +12,12 @@
 			<li><strong>Date:</strong> April 2013 (major refactor in March 2015)</li>
 		</ul>
 
-		<p>As one of the first things I built with node.js I kept it simple, but added a twist to the standard "messaging system" by encrypting all messages locally in the users browser. The encryption key is stored in the URL so by sharing the URL with friends you can securely send messages.</p>
-        <p>When I refactored this project in 2015 I moved from EJS templates to <strong>{{ mustache }}</strong> and from my own custom routing to using <strong>page.js</strong>.</p>
+		<p>As one of the first things I built with node.js I kept it simple, but added a twist to the standard "messaging system" by encrypting all messages locally in the users browser. The encryption key is stored in the URL hash so by sharing the URL with friends you can securely send messages, but the encryption key is never sent to the server / seen by the network.</p>
+        <p>I have refactored this project several times:</p>
+		<ul>
+			<li>I moved from EJS templates to <strong>{{ mustache }}</strong> and from my own custom routing to using <strong>page.js</strong>.</li>
+			<li>I decided to try <strong>ES6</strong> modules / classes and use <strong>Ractive</strong> for templating.</li>
+		</ul>
 		<p>If you would like to see how I have built the website, please review the <a href="https://github.com/joelvardy/chatting.im" title="Open source repository" data-analytics="chatting.im repo">the source code</a> on GitHub.</p>
 
 	</div>
