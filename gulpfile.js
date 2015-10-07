18c0b4c09540f93a2881fe046a9926f96fa77297
Joel Vardy <info@joelvardy.com>
2015-10-07 22:35:30 +0100

    Refactored Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,51 @@
 // Load plugins
 var del = require('del'),
-	gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifyCSS = require('gulp-minify-css'),
-	concat = require('gulp-concat'),
-	jshint = require('gulp-jshint'),
-	jshintStylish = require('jshint-stylish'),
-	uglify = require('gulp-uglify');
-
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 // Styles
 gulp.task('styles', function () {
 
-	return sass('./public/assets/scss/design.scss')
-	.pipe(autoprefixer())
-	.pipe(minifyCSS())
-	.on('error', function (error) {
-		console.error('Error!', error);
-	})
-	.pipe(gulp.dest('./public/assets/minified'));
+    return gulp.src('./public/assets/scss/design.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer())
+        .on('error', function (error) {
+            console.error('Error!', error);
+        })
+        .pipe(gulp.dest('./public/assets/minified'));
 
 });
-
-
-// Lint JavaScript
-gulp.task('lint', function () {
-
-	return gulp.src('public/assets/js/main.js')
-	.pipe(jshint())
-	.pipe(jshint.reporter('jshint-stylish'));
-
-});
-
 
 // JavaScript
 gulp.task('scripts', function () {
 
-	return gulp.src('public/assets/js/**/*.js')
-	.pipe(concat('app.js'))
-	.pipe(uglify())
-	.on('error', function (error) {
-		console.log(error);
-	})
-	.pipe(gulp.dest('./public/assets/minified'));
+    return gulp.src('./public/assets/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .on('error', function (error) {
+            console.error('Error!', error);
+        })
+        .pipe(gulp.dest('./public/assets/minified'));
 
 });
-
 
 // Clean
 gulp.task('clean', function () {
-	del(['./public/assets/minified/*']);
+    del(['./public/assets/minified/*']);
 });
-
 
 // Default task
 gulp.task('default', ['clean'], function () {
-	gulp.start('styles', 'scripts');
+    gulp.start('styles', 'scripts');
 });
 
 // Watch
-gulp.task('watch', function () {
-	gulp.watch('./public/assets/scss/**/*.scss', ['styles']);
-	gulp.watch('./public/assets/js/**/*.js', ['scripts']);
+gulp.task('watch', ['default'], function () {
+    gulp.watch('./public/assets/scss/**/*.scss', ['styles']);
+    gulp.watch('./public/assets/js/**/*.js', ['scripts']);
 });
--- readme.md
+++ readme.md
@@ -22,38 +22,26 @@ npm install
 
 ### SCSS and JavaScript
 
-Ensure packages are installed:
-
-```
-sudo gem install sass
-npm install -g gulp
-```
-
 You can minify JavaScript and compile SCSS by running gulp:
 
 ```
-gulp watch
-```
-
-You can also check JavaScript using:
-
-```
-gulp lint
+gulp # Compile once
+gulp watch # Compile and watch for changes
 ```
 
 ## Application
 
 ### Libraries
 
-Most of the libraries I am using are loaded by Composer (see the `/composer.json` file) - however within the `/application/libraries/Joelvardy` directory there are a few application specific libraries.
+Most of the libraries I am using are loaded by Composer (see the `/composer.json` file) - however within the `/app/libraries/Joelvardy` directory there are a few application specific libraries.
 
 ### Routes
 
-All files in the `/application/routes` directory are loaded into the application.
+All files in the `/app/routes` directory are loaded into the application.
 
 ### Views
 
-The `/application/views` directory contains the HTML with makes up the website, under this folder are project / writing partials.
+The `/app/views` directory contains the HTML with makes up the website, under this folder are project / writing partials.
 
 All requests (which are not valid resources) are routed to the `/public/index.php` file, this file loads all of the routes, the matching route will have a callback which is then executed.
 
